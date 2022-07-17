e458b7bb4f5420b3b151b7f649cbc2c21a4171bc
Abidemi Ope <opeabidemi@gmail.com>
2022-07-17 23:21:53 +0100

    chnages

---
+++ app/screens/Home.tsx
@@ -0,0 +1,13 @@
+import {Text} from 'native-base';
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
